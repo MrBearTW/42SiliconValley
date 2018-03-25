59fd98877894c09cec7120ad035de61c7b866624
Chiwei Perng <cperng@e1z1r7p21.42.us.org>
2018-03-25 09:00:36 -0700

    20180325

--- RUSH01/sudoku_main.1.c
+++ RUSH01/sudoku_main.1.c
@@ -1,7 +1,7 @@
 #include "sudoku_main.h"
 
 
-// i = guess num
+// i = guess num ?
 int		build_board2(int **puzzle, int row, int col, int i)
 {
 	while (i < 9)
@@ -133,17 +133,38 @@ int		main(int argc, char **argv)
 		return (0);
 	}
 
-
+	// get puzzle shape and make sure is 9*9
 	puzzle = ft_create_puzzle(argv);
 	if (puzzle == 0)
 	{
 		return (0);
 	}
 
-
 	if (build_board(puzzle, 0, 0))
 		ft_print_puzzle(puzzle);
 	else
 		write(1, "Error!\n", 7);
 	return (0);
-}
+}/*
+./web 
+"9...7...." 
+"2...9..53"  
+".6..124.." 
+"84...1.9." 
+"5.....8.." 
+".31..4..." 
+"..37..68." 
+".9..5.741" 
+"47......."
+
+9 1 4 3 7 5 2 6 8
+2 8 7 4 9 6 1 5 3
+3 6 5 8 1 2 4 7 9
+8 4 6 5 2 1 3 9 7
+5 2 9 6 3 7 8 1 4
+7 3 1 9 8 4 5 2 6
+1 5 3 7 4 9 6 8 2
+6 9 8 2 5 3 7 4 1
+4 7 2 1 6 8 9 3 5
+
+*/
